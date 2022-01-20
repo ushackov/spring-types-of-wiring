9b415c11550efe8445f31e622c08dc7e9cb276d5
ushackov <ushackov@gmail,com>
2022-01-20 22:56:30 +0300

    Кощей умер

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -1,13 +1,13 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Wood3 {
 
     private final Rabbit4 rabbit;
-
     @Autowired
     public Wood3(Rabbit4 rabbit) {
         this.rabbit = rabbit;
